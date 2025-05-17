922d5c5da9c060f0d1c87c4d57c9ed47812e1f45
smaosn <samsonite8503@gmail.com>
2025-05-17 17:45:16 +0800

    Created Log In Page

---
+++ app/index.jsx
@@ -0,0 +1,85 @@
+import { LinearGradient } from 'expo-linear-gradient';
+import { Image, Pressable, StyleSheet, Text, View } from 'react-native';
+import { TextInput } from 'react-native-web';
+
+const handlePress = () => {
+  console.log("Logged In");
+}
+
+export default function App() {
+  return (
+    <LinearGradient
+      colors={['#5de0e6', '#004aad']}
+      start={{ x: 0.5, y: 0 }}
+      end={{ x: 0.5, y: 1 }}
+      style={styles.container}
+    >
+    <View style = {styles.view}>
+      <Image 
+        source={require("../assets/images/slash-it-logo.png")}
+        resizeMode='contain' 
+      />
+      <Text style = {styles.text}>Username</Text>
+      <TextInput
+        style={styles.textInput}
+        placeholder="Enter your username"
+        placeholderTextColor="#FFF"
+      />
+      <Text style = {styles.text}>Password</Text>
+      <TextInput
+        style={styles.textInput}
+        placeholder="Enter your Password"
+        placeholderTextColor="#FFF"
+      />
+      <Pressable style = {styles.button} onPress={handlePress}>
+        <Text style = {styles.text}>Log In</Text>
+      </Pressable>
+    </View>
+    </LinearGradient>
+    
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  view: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  text: {
+    color: "#FFF",
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  textInput : {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 20,
+    paddingHorizontal: 10,
+    width: '80%',
+    textAlign: 'center',
+  },
+  button: {
+    width: 200,
+    backgroundColor: "steelblue"
+  }
+});
+
+
+
+/*
+const Cat = () => {
+  const name = 'Cat';
+  return <Text>Hello, I am a {name}!</Text>;
+};
+
+export default Cat;
+*/
