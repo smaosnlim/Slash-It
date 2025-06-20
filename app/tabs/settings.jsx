bc0f480d99cc9233b9cae3bfa85d2836a1d97bb1
samson <samsonlim850@gmail.com>
2025-06-20 12:56:42 +0800

    getDoc in settings page working

--- app/tabs/settings.jsx
+++ app/tabs/settings.jsx
@@ -1,15 +1,42 @@
-import { Pressable, StyleSheet, Text, View } from 'react-native';
+import { getAuth } from 'firebase/auth';
+import { doc, getDoc, getFirestore } from 'firebase/firestore';
+import { useEffect, useState } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { app } from '../../backend/firebase';
 
 export default function Settings({navigation}) {
+
+    const [age, setAge] = useState(0);
+    const [occupation, setOccupation] = useState('');
+    const [interests, setInterests] = useState('');
+
+    useEffect(() => {
+        
+        const db = getFirestore(app);
+        const userId = getAuth(app).currentUser.uid;
+        const docRef = doc(db, 'slash-it-users', userId);
+        try {
+            getDoc(docRef).then((docSnap) => {
+                if (docSnap.exists()) {
+                    const data = docSnap.data();
+                    setAge(data.age || 0);
+                    setOccupation(data.occupation || '');
+                    setInterests(data.interests ? data.interests.join(', ') : '');
+                } else {
+                    console.log("No such document!");
+                }
+            })} catch (error) {
+                console.error("Error getting document:", error);
+            }
+        });
+
     return (
         <SafeAreaView style = {styles.outerContainer}>
-            {/*<Header />*/}
             <View style = {styles.container}>
-                <Text> See your settings here</Text>
-                <Pressable style = {styles.button} onPress = {() => navigation.navigate('Home')}>
-                    <Text>Home</Text>
-                </Pressable>
+                <Text>{age}</Text>
+                <Text>{occupation}</Text>
+                <Text>{interests}</Text>
             </View>
         </SafeAreaView>
     )
--- backend/firebase.js
+++ backend/firebase.js
@@ -3,7 +3,6 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { initializeApp } from "firebase/app";
 import { browserLocalPersistence, getReactNativePersistence, initializeAuth } from "firebase/auth";
-//import { getFirestore } from "firebase/firestore";
 import { Platform } from "react-native";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
